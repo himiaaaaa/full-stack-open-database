b040791c3529d53195ca39c136a4d521fff51699
himiaaaaa <126934346+himiaaaaa@users.noreply.github.com>
2023-09-28 12:44:21 +0300

    Task 13.4

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 require('dotenv').config()
-const { Sequelize, QueryTypes, Model, DataTypes } = require('sequelize')
+const { Sequelize, Model, DataTypes } = require('sequelize')
 const express = require('express')
 const app = express()
 
@@ -31,8 +31,12 @@ Note.init({
     modelName: 'note'
 })
 
+Note.sync()
+
 app.get('/api/notes', async (req, res) => {  
     const notes = await Note.findAll()
+
+    console.log(console.log(JSON.stringify(notes, null, 2)))
     res.json(notes)
 })
 
@@ -42,6 +46,18 @@ app.post('/api/notes', async (req, res) => {
         return res.json(note)
     } catch(error) {
         return res.status(400).json({ error })
+    }
+})
+
+app.get('/api/notes/:id', async (req, res) => {
+    const note = await Note.findByPk(req.params.id)
+    if (note) {
+      console.log(note.toJSON())
+      note.important = req.body.important
+      await note.save()
+      res.json(note)
+    } else {
+      res.status(404).end()
     }
 })
 
