5a737a5752c1ca09c03aa9007c376797f11985f4
himiaaaaa <126934346+himiaaaaa@users.noreply.github.com>
2023-09-28 22:25:10 +0300

    Task 13.7

--- blog_exercise_app/controllers/blogs.js
+++ blog_exercise_app/controllers/blogs.js
@@ -2,6 +2,8 @@ const router = require('express').Router()
 
 const { Blog } = require('../models')
 
+
+
 router.get('/', async(req, res) => {
     const blogs = await Blog.findAll()
     console.log(JSON.stringify(blogs, null, 2))
@@ -9,12 +11,8 @@ router.get('/', async(req, res) => {
 })
 
 router.post('/', async(req, res) => {
-    try {
-        const blog = await Blog.create(req.body)
-        return res.json(blog)
-    } catch (error) {
-        return res.status(400).json({ error })
-    }
+    const blog = await Blog.create(req.body)
+    return res.json(blog)
 })
 
 const blogFinder = async (req, res, next) => {
