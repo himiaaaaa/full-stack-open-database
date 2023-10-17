4701ccc957fdce243c1d9cb6f8fca11e8cb6c49b
himiaaaaa <126934346+himiaaaaa@users.noreply.github.com>
2023-10-17 21:52:49 +0300

    Task 13.22

--- blog_exercise_app/controllers/readingLists.js
+++ blog_exercise_app/controllers/readingLists.js
@@ -7,6 +7,11 @@ router.get('/', async(req, res) => {
     res.json(readingList)
 })
 
+router.get('/:id', async(req, res) => {
+    const readingList = await ReadingLists.findByPk(req.params.id)
+    res.json(readingList)
+})
+
 router.post('/', async(req, res) => {
     try {
         const body = req.body
@@ -18,6 +23,17 @@ router.post('/', async(req, res) => {
         if (!user) return res.status(404).json({ error: 'user not found' })
 
         const readingList = await ReadingLists.create(body)
+        res.json(readingList)
+    } catch (error) {
+        return res.status(400).json({ error })
+    } 
+})
+
+router.put('/:id', async(req, res) => {
+    try {
+        const readingList = await ReadingLists.findByPk(req.params.id)
+        readingList.readState = req.body.read
+        await readingList.save()
         res.json(readingList)
     } catch (error) {
         return res.status(400).json({ error })
